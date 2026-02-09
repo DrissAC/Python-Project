cb5aa5db951a2af26ac44db5522fb21aeeb1376a
Driss AC <74766435+DrissAC@users.noreply.github.com>
2026-02-09 01:42:26 +0100

    Game update, you can choose the n number (guess the number between 1 and n), you now have the choice to continue playing again or not

--- DevineLeNombre.py
+++ DevineLeNombre.py
@@ -1,22 +1,31 @@
 import random
 
-secret_number = random.randint(1, 10)
+
 print(f"\n--- Bienvenue au jeu 'Devine le Nombre' ---")
-attemps = int (input("Combien d'essais voulez-vous ? : "))
+while True:
+    choose_secret_number = int(input("Choississez un nombre secret : ").strip())
+    print(f"Vous avez choisi {choose_secret_number}, je vais penser a un nombre entre 1 et {choose_secret_number}, a vous de le deviner !")
+    secret_number = random.randint(1, choose_secret_number)
+    attemps = int (input("Combien d'essais voulez-vous ? : "))
 
-print(f"Je pense à un nombre entre 1 et 10, vous avez {attemps} essais !")
+    print(f"Je pense à un nombre entre 1 et {choose_secret_number}, vous avez {attemps} essais !")
 
-while attemps > 0: 
-    guess = int(input("Pouvez-vous le devinez ?: "))
-    if guess == secret_number:
-        print("Bravo, vous avez gagné")
+    while attemps > 0: 
+        guess = int(input("Pouvez-vous le devinez ?: "))
+        if guess == secret_number:
+            print("Bravo, vous avez gagné")
+            break
+        elif guess > secret_number:
+            print("C'est moins")
+            attemps -= 1
+        else:
+            print("C'est plus")
+            attemps -= 1
+
+    if attemps == 0:
+        print("Dommage, vous avez perdu, le nombre secret était", secret_number)
+
+    play_again = input("Voulez-vous jouer encore ? (o/n) : ").strip().lower()
+    if play_again != "o":
+        print("Merci d'avoir joué !")
         break
-    elif guess > secret_number:
-        print("C'est moins")
-        attemps -= 1
-    else:
-        print("C'est plus")
-        attemps -= 1
-
-if attemps == 0:
-    print("Dommage, vous avez perdu, le nombre secret était", secret_number)
